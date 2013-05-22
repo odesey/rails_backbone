09ad66d66a6789f4e82131d32d953bd18c44bb3f
odesey <odesey@gmail.com>
2013-05-22 14:29:00 -0400

    generated new rails app to host my own users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBackbone::Application.config.secret_token = 'aa7e5821da171454030c49bf5ab7ee7d64635ad08f9886ceee6892988df1a19951f25be32c88bdbcf5babef14a9a74d9900e02176455b674cb064233fd4b2b95'
